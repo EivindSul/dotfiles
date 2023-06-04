63d2956e5af2ef93ff0ff35d17049eef9aabe5bd
EivindSul <eivindsulen2002@gmail.com>
2023-06-04 17:08:11 +0200

    added nvim config

---
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,22 @@
+-- Themes
+return {
+	"typicode/bg.nvim",
+
+	"ellisonleao/gruvbox.nvim",
+
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+	},
+
+	{
+		"rose-pine/nvim",
+		name = "rose-pine",
+	},
+
+	"mhartington/oceanic-next",
+
+	"sainnhe/everforest",
+
+	"savq/melange-nvim"
+}

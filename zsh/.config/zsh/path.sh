c042d81e81f4ee4881855e7118ba6a6fcd6dc4a7
EivindSul <eivindsulen2002@gmail.com>
2024-02-17 20:32:32 +0100

    added everforest colorscheme for zellij

--- zsh/.config/zsh/path.sh
+++ zsh/.config/zsh/path.sh
@@ -2,4 +2,5 @@ export PATH="/usr/local/anaconda3/bin:$PATH"
 export PATH="/opt/homebrew/anaconda3/bin:$PATH"
 export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 export PATH="/opt/homebrew/opt/sqlite/bin:$PATH"
+export PATH="/Users/eivind/.cargo/bin:$PATH"
 

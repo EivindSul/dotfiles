dbd48dec4c7c2f70d1b1168b1177ae35eb52b95c
Eivind Sulen <eivindsulen2002@gmail.com>
2024-05-14 11:55:12 +0200

    added alias for beammp-launcher on linux. removed conda from fish

---
+++ fish/.config/fish/functions/beammp.fish
@@ -0,0 +1,4 @@
+function beammp --wraps='bash ~/programs/games/beam/BeamMP-Linux/beammp-linux.sh' --description 'alias beammp bash ~/programs/games/beam/BeamMP-Linux/beammp-linux.sh'
+  bash ~/programs/games/beam/BeamMP-Linux/beammp-linux.sh $argv
+        
+end

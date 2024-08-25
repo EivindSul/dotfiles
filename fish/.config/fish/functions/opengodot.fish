aa5fd82cb4e20f54b0317b472f2d605f68d90747
Eivind Sulen <eivindsulen2002@gmail.com>
2024-08-25 17:51:58 +0200

    Fish: Added function to launch godot engine in wayland, forcing openGL

--- fish/.config/fish/functions/opengodot.fish
+++ fish/.config/fish/functions/opengodot.fish
@@ -1,4 +1,4 @@
-function opengodot --wraps='~/programs/godot-4.3/Godot_v4.3-dev5_linux.arm64 --display-driver wayland' --description 'alias opengodot ~/programs/godot-4.3/Godot_v4.3-dev5_linux.arm64 --display-driver wayland'
-  ~/programs/godot-4.3/Godot_v4.3-dev5_linux.arm64 --display-driver wayland $argv
+function opengodot --wraps='~/programs/godot-4.3/Godot_v4.3-beta2_linux.arm64 --display-driver wayland --rendering-driver opengl3' --description 'alias opengodot ~/programs/godot-4.3/Godot_v4.3-dev5_linux.arm64 --display-driver wayland'
+  ~/programs/godot-4.3/Godot_v4.3-beta2_linux.arm64 --display-driver wayland --rendering-driver opengl3 $argv
         
 end

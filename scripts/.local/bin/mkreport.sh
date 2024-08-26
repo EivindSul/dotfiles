3f12161cc57b65920d44f84524e0de7b6cf739c8
Eivind Sulen <eivindsulen2002@gmail.com>
2024-08-26 14:09:00 +0200

    Made a script to make a folder with a latex report template
    This template resides at ~/Documents/skole/report-sample.
    If the sample is not found, or the file path you are writing to is wrong, it will fail.

---
+++ scripts/.local/bin/mkreport.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [ -z $1 ] ; then
+    echo "You need to provide a name for the folder."
+    exit 1
+fi
+
+SAMPLE_DIR=$HOME/Documents/skole/report-sample
+
+if [ ! -d $HOME/Documents/skole/report-sample ] ; then 
+    echo "Report sample not found at $SAMPLE_DIR, aborting."
+    exit 1
+fi
+
+REPORT_PATH=$(pwd)/$1
+
+if [ -e $REPORT_PATH ] ; then
+    echo "File or folder already exists at $REPORT_PATH. Aborting."
+    exit 1
+fi
+
+cp -r $SAMPLE_DIR $REPORT_PATH || exit 1
+
+if [ ! -d $REPORT_PATH ] ; then
+    echo "Something went wrong and the report has not been created."
+    exit 1
+else
+    echo "Made report directory at $REPORT_PATH."
+    exit 0
+fi

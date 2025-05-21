7bc485056c22b75eb9376bd1b607337095e09740
Eivind Sulen <eivindsulen2002@gmail.com>
2025-05-21 20:51:07 +0200

    hypr: add corne/colemak binds. Add device specific config based on hostname

--- hyprland/.config/hypr/device-specific.sh
+++ hyprland/.config/hypr/device-specific.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$(hostname)" = "asahi" ]; then
-    hyprctl keyword source ~/.config/hypr/hypr-mac.conf
+    hyprctl keyword source ~/.config/hypr/hypr-mac.conf 
+    # &&
+    # hyprctl hyprpaper listactive | awk '{print $1","$3}' | xargs -I {} hyprctl hyprpaper wallpaper {}
 fi
